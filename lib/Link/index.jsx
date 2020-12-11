af0cf6502f77f4ba55494e222f203d0f6baa8ff7
Graham McCulloch <admin@grahammcculloch.co.uk>
2020-12-11 15:19:17 +0700

    Fix Link component references
    Change-type: patch
    Signed-off-by: Graham McCulloch <graham@balena.io>

--- lib/Link/index.jsx
+++ lib/Link/index.jsx
@@ -4,16 +4,50 @@
  * Proprietary and confidential.
  */
 
+import React from 'react'
+import _ from 'lodash'
 import {
 	withRouter
 } from 'react-router-dom'
 import {
 	Link as InnerLink
 } from './Link'
-
-export {
-	getLinkProps,
-	linkComponentOverride
-} from './Link'
+import {
+	isRelativeUrl,
+	isLocalUrl,
+	toRelativeUrl
+} from '../services/helpers'
 
 export const Link = withRouter(InnerLink)
+
+export const getLinkProps = (href) => {
+	if (isRelativeUrl(href)) {
+		return {
+			append: href.replace(/^\//, '')
+		}
+	}
+	return (isLocalUrl(href))
+		? {
+			append: toRelativeUrl(href).replace(/^\//, '')
+		}
+		: {
+			to: href,
+			blank: true
+		}
+}
+
+export const linkComponentOverride = ({
+	blacklist
+}) => {
+	return ({
+		href, ...rest
+	}) => {
+		if (_.some(blacklist, (url) => {
+			return href.match(url)
+		})) {
+			return null
+		}
+		const linkProps = getLinkProps(href)
+		return <Link {...rest} {...linkProps} />
+	}
+}
