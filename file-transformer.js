3cd3df6d1dfa03212b452adf3e9be93689fc6153
Kakhaber <kaxabernet@gmail.com>
2021-04-08 17:57:28 +0400

    License header added

--- file-transformer.js
+++ file-transformer.js
@@ -1,3 +1,8 @@
+/*
+ * Copyright (C) Balena.io - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited.
+ * Proprietary and confidential.
+ */
 const path = require('path');
 
 module.exports = {
