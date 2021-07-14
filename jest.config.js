14ddc53042a0d6e9adf5acd83849520372e368f3
Graham McCulloch <admin@grahammcculloch.co.uk>
2021-07-14 07:33:39 +0000

    Fix jest config
    Change-type: patch
    Signed-off-by: Graham McCulloch <graham@balena.io>

--- jest.config.js
+++ jest.config.js
@@ -7,7 +7,7 @@
 const base = require('@balena/jellyfish-config/config/jest.config')
 
 module.exports = {
-	preset: base.preset,
+	...base,
 	testEnvironment: 'jsdom',
 	transform: {
 		'\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga|css)$':
