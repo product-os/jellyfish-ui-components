86766186528d2a68921f13375bae27af97d9c781
Lucian Buzzo <lucian.buzzo@gmail.com>
2021-09-23 10:31:26 +0100

    Simplify timeline pagination
    The HOC should be in control of paginating data, not the timeline
    
    Change-type: major
    Signed-off-by: Lucian Buzzo <lucian.buzzo@gmail.com>

--- lib/InfiniteList.tsx
+++ lib/InfiniteList.tsx
@@ -5,7 +5,7 @@
  */
 
 import React from 'react';
-import { Box } from 'rendition';
+import { Box, BoxProps } from 'rendition';
 import styled from 'styled-components';
 
 const ScrollArea = styled(Box)`
@@ -13,12 +13,11 @@ const ScrollArea = styled(Box)`
 	height: 100%;
 `;
 
-interface InfiniteListProps {
-	fillMaxArea: boolean;
+const TRIGGEROFFSET = 200;
+
+interface InfiniteListProps extends BoxProps {
 	onScrollBeginning?: () => void;
 	onScrollEnding?: () => void;
-	processing?: boolean;
-	triggerOffset?: number;
 }
 
 export class InfiniteList extends React.Component<InfiniteListProps> {
@@ -29,69 +28,20 @@ export class InfiniteList extends React.Component<InfiniteListProps> {
 
 		this.handleRef = this.handleRef.bind(this);
 		this.handleScroll = this.handleScroll.bind(this);
-		this.queryForMoreIfNecessary = this.queryForMoreIfNecessary.bind(this);
-	}
-
-	componentDidMount() {
-		const { fillMaxArea } = this.props;
-		if (fillMaxArea) {
-			this.queryForMoreIfNecessary();
-		}
-	}
-
-	componentDidUpdate() {
-		const { fillMaxArea } = this.props;
-		if (fillMaxArea) {
-			this.queryForMoreIfNecessary();
-		}
-	}
-
-	// QueryForMoreIfNecessary runs a query if the component
-	// is not a scrollable container. This is to account
-	// for a situation with the timeline component
-	// where only one or two messages exist alongside
-	// a page of whispers. In this situation, if a user
-	// toggles off the whispers, we need to trigger another
-	// query. If we don't do this, there may be other
-	// messages further up the timeline that the user
-	// cannot load because they cannot scroll
-	queryForMoreIfNecessary() {
-		const { onScrollBeginning, onScrollEnding, processing } = this.props;
-		if (processing) {
-			return;
-		}
-		const { clientHeight, scrollHeight } = this.scrollArea;
-		const noScrollBar = clientHeight === scrollHeight;
-		if (noScrollBar) {
-			if (onScrollBeginning) {
-				onScrollBeginning();
-			}
-			if (onScrollEnding) {
-				onScrollEnding();
-			}
-		}
 	}
 
 	handleScroll() {
-		const {
-			processing,
-			onScrollBeginning,
-			onScrollEnding,
-			triggerOffset = 200,
-		} = this.props;
+		const { onScrollBeginning, onScrollEnding } = this.props;
 
-		if (processing) {
-			return;
-		}
 		const { scrollTop, scrollHeight, offsetHeight } = this.scrollArea;
 
-		if (scrollTop < triggerOffset && onScrollBeginning) {
+		if (scrollTop < TRIGGEROFFSET && onScrollBeginning) {
 			onScrollBeginning();
 		}
 
 		const scrollOffset = scrollHeight - (scrollTop + offsetHeight);
 
-		if (scrollOffset > triggerOffset) {
+		if (scrollOffset > TRIGGEROFFSET) {
 			return;
 		}
 
